a701d0e1c1982600840991d58dc122623c99bb2f
chenzhifengla <chen1511@foxmail.com>
2016-12-10 12:43:22 +0800

    forth

--- cmake-build-debug/CMakeFiles/netfilter.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/netfilter.dir/DependInfo.cmake
@@ -1,21 +1,7 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "C"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_C
-  "/cygdrive/d/Users/yingzi/ClionProjects/netfilter/client/client.c" "/cygdrive/d/Users/yingzi/ClionProjects/netfilter/cmake-build-debug/CMakeFiles/netfilter.dir/client/client.c.o"
-  "/cygdrive/d/Users/yingzi/ClionProjects/netfilter/src/alarmDetection.c" "/cygdrive/d/Users/yingzi/ClionProjects/netfilter/cmake-build-debug/CMakeFiles/netfilter.dir/src/alarmDetection.c.o"
-  "/cygdrive/d/Users/yingzi/ClionProjects/netfilter/src/communication.c" "/cygdrive/d/Users/yingzi/ClionProjects/netfilter/cmake-build-debug/CMakeFiles/netfilter.dir/src/communication.c.o"
-  "/cygdrive/d/Users/yingzi/ClionProjects/netfilter/src/dealConf.c" "/cygdrive/d/Users/yingzi/ClionProjects/netfilter/cmake-build-debug/CMakeFiles/netfilter.dir/src/dealConf.c.o"
-  "/cygdrive/d/Users/yingzi/ClionProjects/netfilter/src/netFilter.c" "/cygdrive/d/Users/yingzi/ClionProjects/netfilter/cmake-build-debug/CMakeFiles/netfilter.dir/src/netFilter.c.o"
-  "/cygdrive/d/Users/yingzi/ClionProjects/netfilter/threadTest/kthread.c" "/cygdrive/d/Users/yingzi/ClionProjects/netfilter/cmake-build-debug/CMakeFiles/netfilter.dir/threadTest/kthread.c.o"
-  )
-set(CMAKE_C_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
-  )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
