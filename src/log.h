c038a76d5e23c382c6e5c48289d88202c0c71a74
chenzhifengla <chen1511@foxmail.com>
2016-12-27 20:16:43 +0800

    取消了在初始化netfilter时发送netlink消息

--- src/log.h
+++ src/log.h
@@ -3,6 +3,8 @@
  */
 //#ifdef LOGKERNEL
 
+extern char mymessagebuf[1000];  // 放置缓冲区声明
+
 #define DEBUG(...) printk(KERN_EMERG "DEBUG:" __VA_ARGS__)
 #define INFO(...) printk(KERN_EMERG "INFO:" __VA_ARGS__)
 #define WARNING(...) printk(KERN_EMERG "WARING:" __VA_ARGS__)
