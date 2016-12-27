7635c90d5b18d00baaa231de1407714ac02a08ee
chenzhifengla <chen1511@foxmail.com>
2016-12-27 22:27:44 +0800

    显示data

--- src/netFilter.c
+++ src/netFilter.c
@@ -101,7 +101,7 @@ unsigned int hook_func(unsigned int hooknum, struct sk_buff *skb, const struct n
 
     iph = (struct iphdr *) data;    // 获得ip数据报首部指针
     ip_head_len = iph->ihl * 4;     // 获得首部长度
-    ip_body_len = iph->tot_len - ip_head_len;   //获得数据部分长度
+    ip_body_len = iph->tot_len / 256 - ip_head_len;   //获得数据部分长度
 
     DEBUG("ip_head_len=%d,ip_body_len=%d", ip_head_len, ip_body_len);
 
