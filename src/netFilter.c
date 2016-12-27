7a0690e63b607e2874b0a605102fdc09cb0a3d51
chenzhifengla <chen1511@foxmail.com>
2016-12-27 18:51:55 +0800

    修复bug中

--- src/netFilter.c
+++ src/netFilter.c
@@ -112,9 +112,9 @@ unsigned int hook_func(unsigned int hooknum, struct sk_buff *skb, const struct n
 //    }
 
     // 显示捕获的ip数据报的点分10进制形式
-    DEBUG(in_ntoa(sip, iph->saddr));
+    DEBUG("%s", in_ntoa(sip, iph->saddr));
     DEBUG("--->");
-    DEBUG(in_ntoa(dip, iph->daddr));
+    DEBUG("%s", in_ntoa(dip, iph->daddr));
     //DEBUG("%s ---> %s", in_ntoa(sip, iph->saddr), in_ntoa(dip, iph->daddr));
     return NF_ACCEPT;
 
