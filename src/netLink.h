55ac83ce6352ae78185c354b98a99fef6edfb0ce
chenzhifengla <chen1511@foxmail.com>
2016-12-27 17:17:16 +0800

    修复bug中

--- src/netLink.h
+++ src/netLink.h
@@ -3,10 +3,10 @@
  */
 
 #define NETLINK_TEST 20 // 自定义的netlink协议号
-#define NETLINK_TEST_CONNECT 12  // 自定义的netlink客户端发送连接请求时type
-#define NETLINK_TEST_DISCONNECT 23   // 自定义的netlink客户端发送断开连接请求时type
-#define NETLINK_TEST_COMMAND 45  // 自定义的netlink客户端发送的指令
-#define NETLINK_TEST_REPLY  56   // 自定义的netlink内核回复的消息类型
+#define NETLINK_TEST_CONNECT 0x10  // 自定义的netlink客户端发送连接请求时type
+#define NETLINK_TEST_DISCONNECT 0x11   // 自定义的netlink客户端发送断开连接请求时type
+#define NETLINK_TEST_COMMAND 0x12  // 自定义的netlink客户端发送的指令
+#define NETLINK_TEST_REPLY  0x13   // 自定义的netlink内核回复的消息类型
 
 int createNetlink(void);
 
