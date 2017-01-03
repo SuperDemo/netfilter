b97ff75570586965a3d1ce4a6c2a8b34edcf68a4
chenzhifengla <chen1511@foxmail.com>
2017-01-03 16:23:57 +0800

    修改pyclient

--- client/pyclient/pyclient.py
+++ client/pyclient/pyclient.py
@@ -3,10 +3,10 @@ import sys
 import os
 import socket
 import struct
-from impacket import ImpactDecoder, ImpactPacket
 
-NLMSG_NOOP = 0x1
+
 NETLINK_TEST = 20
+NETLINK_TEST_CONNECT = 0x10
 
 def main():
 
@@ -15,7 +15,7 @@ def main():
 
     # 设置nlmsghdr的信息
     nlmsg_len = 0  # 头部长度, __u32
-    nlmsg_type = NLMSG_NOOP  # 消息主体, __u16
+    nlmsg_type = 0x10  # 消息主体, __u16
     nlmsg_flags = 0 # 额外参数, __u16
     nlmsg_seq = 0   # 序列号, __u32
     nlmsg_pid = os.getpid()  # 进程pid, __u32
@@ -29,11 +29,12 @@ def main():
 
 
     # 打包参数，int or long,
-    nlmsg = struct.pack('IHHII%ds' % len(nlmsg_mymsg), nlmsg_len, nlmsg_type, nlmsg_flags, nlmsg_seq, nlmsg_pid, str(nlmsg_mymsg))
+    # nlmsg = struct.pack('IHHII%ds' % len(nlmsg_mymsg), nlmsg_len, nlmsg_type, nlmsg_flags, nlmsg_seq, nlmsg_pid, str(nlmsg_mymsg))
+    nlmsg = struct.pack('IHHII', nlmsg_len, nlmsg_type, nlmsg_flags, nlmsg_seq, nlmsg_pid)
     # print type(nlmsg)
     # print ":".join("{:02x}".format(ord(c)) for c in nlmsg)
-
     sock.send(nlmsg)
+
     while True:
         data = sock.recv(1024)
         print data
