73a8bc402a7b918382efd82ff383ce5c4c9aeaa0
SuperDemo <bupt2014>
2017-03-01 05:32:58 +0800

    第一次提交

--- client/pyclient/pyclient (复件).py
+++ client/pyclient/pyclient (复件).py
@@ -20,7 +20,7 @@ def main():
     nlmsg_seq = 0   # 序列号, __u32
     nlmsg_pid = os.getpid()  # 进程pid, __u32
 
-    nlmsg_mymsg = "drop\0"    # 用户消息
+    nlmsg_mymsg = "testpyclien\0"    # 用户消息
 
     # 重新设置长度
     # nlmsg_len = sys.getsizeof(nlmsg_len) + sys.getsizeof(nlmsg_type) +\
@@ -33,9 +33,9 @@ def main():
 
 
     # 打包参数，int or long,
-    #nlmsg = struct.pack('IHHII%ds' % len(nlmsg_mymsg), nlmsg_len, nlmsg_type, nlmsg_flags, nlmsg_seq, nlmsg_pid, str(nlmsg_mymsg))
+    # nlmsg = struct.pack('IHHII%ds' % len(nlmsg_mymsg), nlmsg_len, nlmsg_type, nlmsg_flags, nlmsg_seq, nlmsg_pid, str(nlmsg_mymsg))
     nlmsg = struct.pack('IHHII', nlmsg_len, nlmsg_type, nlmsg_flags, nlmsg_seq, nlmsg_pid)
-    #print type(nlmsg)
+    # print type(nlmsg)
     #print ":".join("{:02x}".format(ord(c)) for c in nlmsg)
     sock.send(nlmsg)
 
@@ -44,36 +44,9 @@ def main():
         # data的第一位为nlmsg_len，表示消息的长度，data前16字节为nlmsg的消息头
         # print "len=" + str(ord(data[0]))
         # print ":".join("{:02x}".format(ord(c)) for c in data)
-        #the following message from kernel
-        kdata=data[16:ord(data[0])]
-        #print data[16:ord(data[0])]
-        print "k:", kdata
-        hdata=handledata(kdata)
-        print "h:", hdata
-
-        nlmsg_type = 0x12  # 消息主体, __u16
-        nlmsg_replay = struct.pack('IHHII%ds' % len(nlmsg_mymsg), nlmsg_len, nlmsg_type, nlmsg_flags, nlmsg_seq, nlmsg_pid, str(nlmsg_mymsg))
+        print data[16:ord(data[0])]
 
 
-        if hdata is 'B':
-            print "I have sent a message."
-            sock.send(nlmsg_replay)
-
-        #DEBUG:UDP:"10.108.166.130":51891 ---> "10.108.167.101":8888::
-        # <?xml version='1.0' encoding='gb2312' ?><note id='3'><title>ProblemAck</title>
-        # <message>A5</message></note>
-
-
-def handledata(example = ''):
-    """"""
-    leftdex = example.find(r"<message>")
-    rightdex = example.find(r"</message>")
-    if leftdex < 0 or rightdex < 0:
-        print "fail to parse the event message\n"
-        return
-    else:
-        emessage = example[leftdex+9:rightdex]
-        return emessage
 
 if __name__ == '__main__':
     main()
